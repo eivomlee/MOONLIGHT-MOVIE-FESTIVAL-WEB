a160bec8949965b276f1108f8aaf9ca3f91095dd
Chriso <qudcksrlf1@gmail.com>
2021-08-13 14:56:03 +0900

    vignettes update

--- src/js/main_index.js
+++ src/js/main_index.js
@@ -1,3 +1,4 @@
+//이벤트 리스너 선언
 window.onload = function () {
     const logo_btn = document.querySelector(".logo_btn");
     const home_btn = document.querySelector(".home_btn");
@@ -11,6 +12,7 @@ window.onload = function () {
     searchButton.addEventListener("click", activeSearch);
 }
 
+//화면 스크롤 이벤트
 function move_scroll(event) {
     event.preventDefault();
     window.scrollTo({
@@ -19,6 +21,7 @@ function move_scroll(event) {
     });
 }
 
+//로딩마다 랜덤 배경화면 불러오기
 function random_backgroundImg() {
     const img_container = document.querySelector(".main_index");
     const imgs = ["ex_image1.jpg", "ex_image2.PNG",
@@ -28,10 +31,13 @@ function random_backgroundImg() {
     img_container.style.backgroundImage = `url("src/img/${imgs[random_number]}")`;
     img_container.style.backgroundSize = "100% 100%";
     img_container.style.repeat = "no-repeat";
+    img_container.style.boxShadow = "inset 0 0 100px black";
 }
 
+//검색창 이벤트
 function activeSearch(event) {
     event.preventDefault();
     const searchInput = document.querySelector(".search_input");
     searchInput.classList.toggle("active");
 }
+
